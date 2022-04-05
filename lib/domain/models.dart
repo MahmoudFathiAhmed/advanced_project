a40189028d945e5026677ed9ac9c71370918eef1
MahmoudFathiAhmed <zanoon.dx@gmail.com>
2022-04-05 23:38:08 +0200

    perform extensions

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -13,3 +13,27 @@ class SliderViewObject{
   int currentIndex;
   SliderViewObject(this.sliderObject, this.numberOfSlides, this.currentIndex);
 }
+
+//login models
+class Customer{
+String id;
+String name;
+int numOfNotifications;
+
+Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts{
+  String phone;
+  String email;
+  String link;
+
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication{
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(this.customer, this.contacts);
+}
