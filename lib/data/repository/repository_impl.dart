eeb62863b580dd6f9f0146844139d55b04ea376c
MahmoudFathiAhmed <zanoon.dx@gmail.com>
2022-04-06 18:05:37 +0200

    api error handling

---
+++ lib/data/repository/repository_impl.dart
@@ -0,0 +1,42 @@
+import 'package:advanced_project/data/data_source/remote_data_source.dart';
+import 'package:advanced_project/data/mapper/mapper.dart';
+import 'package:advanced_project/data/network/error_handler.dart';
+import 'package:advanced_project/data/network/failure.dart';
+import 'package:advanced_project/data/network/network_info.dart';
+import 'package:advanced_project/data/network/requests.dart';
+import 'package:advanced_project/domain/model/models.dart';
+import 'package:advanced_project/domain/repository/repository.dart';
+import 'package:dartz/dartz.dart';
+
+class RepositoryImpl implements Repository{
+  final RemoteDataSource _remoteDataSource;
+  final NetworkInfo _networkInfo;
+  RepositoryImpl(this._remoteDataSource, this._networkInfo);
+  @override
+  Future<Either<Failure, Authentication>> login(LoginRequest loginRequest) async{
+
+    if(await _networkInfo.isConnected){
+      //it's connected to the internet, it's safe to call API
+      try{
+        final response  = await _remoteDataSource.login(loginRequest);
+        if (response.status==ApiInternalStatus.SUCCESS){
+          //success
+          //return either right
+          //return data
+          return Right(response.toDomain());
+        }else{
+          //failure -- business error
+          //return either left
+          return Left(Failure(ApiInternalStatus.FAILURE, response.message??ResponseMessage.DEFAULT));
+        }
+      }catch(error){
+        return Left(ErrorHandler.handle(error).failure);
+      }
+    }else{
+      //return internet connection error
+      //return either left
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
+    }
+  }
+
+}
